b824f208af43bc411086a943c93cb07f51679ca8
megaconfidence <cokoghenun@gmail.com>
2025-06-20 22:16:36 +0100

    refactor: Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
-import { Container, getContainer, loadBalance } from '@cloudflare/containers';
+import { Container, loadBalance } from '@cloudflare/containers';
 
 export class WifskiContainer extends Container {
 	defaultPort = 8080;
 	sleepAfter = '5m';
+
 	override onStart() {
 		console.log('Container successfully started');
 	}
-
 	override onStop() {
 		console.log('Container successfully shut down');
 	}
-
 	override onError(error: unknown) {
 		console.log('Container error:', error);
 	}
@@ -18,16 +17,7 @@ export class WifskiContainer extends Container {
 
 export default {
 	async fetch(request: Request, env): Promise<Response> {
-		const { hostname, pathname } = new URL(request.url);
-		const isLocal = hostname === 'localhost' ? true : false;
-
-		if (isLocal) {
-			return await fetch('http://localhost:8080' + pathname, request);
-		}
-
 		let container = await loadBalance(env.WIFSKI_CONTAINER, 3);
 		return await container.fetch(request);
-
-		// return await getContainer(env.WIFSKI_CONTAINER).fetch(request);
 	},
 } satisfies ExportedHandler<Env>;
